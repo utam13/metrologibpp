012124e1832df7ef76b6e9517108b1c265ecf345
utam13 <rustam.blacknetto@gmail.com>
2022-11-04 01:16:31 +0800

    update formulir uttp peserta

--- application/views/page/formulir_uttppeserta.php
+++ application/views/page/formulir_uttppeserta.php
@@ -80,7 +80,7 @@
                                                         <label class="col-sm-3 control-label">Jumlah</label>
                                                         <div class="col-sm-3">
                                                             <div class="input-group">
-                                                                <input type="number" class="form-control block-specialchar" name="jml" id="jml" value="<?= $jml;?>" min=1 max=999 autocomplete="off" required />
+                                                                <input type="number" class="form-control block-specialchar" name="jml" id="jml" value="<?= $jml;?>" min=1 max=999 autocomplete="off" />
                                                                 <span class="input-group-addon">unit</span>
                                                             </div>
                                                         </div>
